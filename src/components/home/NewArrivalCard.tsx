82f919b56f270d47513ac08df298e906c9769a52
Padillahirpan <padillahirpan8@gmail.com>
2025-02-10 20:09:10 +0700

    feat: adjust product card

--- src/components/home/NewArrivalCard.tsx
+++ src/components/home/NewArrivalCard.tsx
@@ -1,12 +1,18 @@
+import { Product } from "@/types/type";
 import banner_1 from "../../assets/images/banner_1.jpg";
+import { rupiah } from "@/libs/utils";
 
-const NewArrivalCard = () => {
+interface NewArrivalCardProps {
+  product: Product;
+}
+
+const NewArrivalCard = ({ product }: NewArrivalCardProps) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white">
       <img className="w-full" src={banner_1} />
       <div className="px-6 py-4">
-        <div className="font-bold text-xl mb-2">Test 1234</div>
-        <p className="text-gray-700 text-base">Harga Murah</p>
+        <div className="font-bold text-xl mb-2">{product.name}</div>
+        <p className="text-gray-700 text-base">{rupiah(product.price)}</p>
       </div>
     </div>
   );
--- src/components/shared/NewArrivalProduct.tsx
+++ src/components/shared/NewArrivalProduct.tsx
@@ -1,46 +1,17 @@
+import { Product } from "@/types/type";
 import NewArrivalCard from "../home/NewArrivalCard";
 import { Button } from "../ui/button";
 
-const NewArrivalProduct = () => {
-  const cards = [
-    {
-      title: "Product 1",
-      description: "This is the description for product 1.",
-      imageUrl: "https://via.placeholder.com/150",
-    },
-    {
-      title: "Product 2",
-      description: "This is the description for product 2.",
-      imageUrl: "https://via.placeholder.com/150",
-    },
-    {
-      title: "Product 3",
-      description: "This is the description for product 3.",
-      imageUrl: "https://via.placeholder.com/150",
-    },
-    {
-      title: "Product 4",
-      description: "This is the description for product 4.",
-      imageUrl: "https://via.placeholder.com/150",
-    },
-    {
-      title: "Product 5",
-      description: "This is the description for product 5.",
-      imageUrl: "https://via.placeholder.com/150",
-    },
-  ];
+interface ProductProps {
+  products?: Product[];
+}
 
+const NewArrivalProduct = ({ products = [] }: ProductProps) => {
   return (
     <div className="flex flex-col gap-4 items-center">
       <div className="flex flex-row gap-4 overflow-x-auto p-4">
-        {cards.map((card, index) => (
-          <NewArrivalCard />
-          // <Card
-          //  key={index}
-          //  title={card.title}
-          //  description={card.description}
-          //  imageUrl={card.imageUrl}
-          // />
+        {products.map((card, index) => (
+          <NewArrivalCard key={index} product={card} />
         ))}
       </div>
       <div>
